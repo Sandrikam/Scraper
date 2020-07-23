964f6280bad4fefaf0a3839e36e5be5fb728a56d
sandrika <sandromaisuradze32@gmail.com>
2020-07-23 16:29:41 +0400

    Added Coments to the code

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
-
+//including scraping library
 include('simple_html_dom.php');
 
 $webUrl = "https://www.apple.com/";
 
 $html = file_get_html($webUrl);
 
-//ty-grid-list__image
 $urls = array();
 foreach($html -> find('.ac-gn-content')as $postDiv){
 
-    // echo '<pre>';
-
-    // die(var_dump($postDiv));
-
- //_ngcontent-sc15
+    
  foreach($postDiv->find('a') as $a)
  {
     //echo $a-> attr['href']."<br>";
